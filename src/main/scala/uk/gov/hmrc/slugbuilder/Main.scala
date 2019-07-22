514cfd62505fae328cdf942e88aeb0101a9c1ba7
chotai <sunny.chotai@digital.hmrc.gov.uk>
2019-07-22 13:31:18 +0100

    BDOG-301 Remove unnecessary laziness of vals

--- src/main/scala/uk/gov/hmrc/slugbuilder/Main.scala
+++ src/main/scala/uk/gov/hmrc/slugbuilder/Main.scala
@@ -27,21 +27,21 @@ import scala.language.postfixOps
 
 object Main {
 
-  private lazy val slugRunnerVersion   = EnvironmentVariables.slugRunnerVersion.getOrExit
-  private lazy val artifactoryUri      = EnvironmentVariables.artifactoryUri.getOrExit
-  private lazy val artifactoryUsername = EnvironmentVariables.artifactoryUsername.getOrExit
-  private lazy val artifactoryPassword = EnvironmentVariables.artifactoryPassword.getOrExit
-  private lazy val jdkFileName         = EnvironmentVariables.jdkFileName.getOrExit
-  private lazy val slugRuntimeJavaOpts = EnvironmentVariables.slugRuntimeJavaOpts
-  private val environmentVariables     = EnvironmentVariables.all
+  private val slugRunnerVersion    = EnvironmentVariables.slugRunnerVersion.getOrExit
+  private val artifactoryUri       = EnvironmentVariables.artifactoryUri.getOrExit
+  private val artifactoryUsername  = EnvironmentVariables.artifactoryUsername.getOrExit
+  private val artifactoryPassword  = EnvironmentVariables.artifactoryPassword.getOrExit
+  private val jdkFileName          = EnvironmentVariables.jdkFileName.getOrExit
+  private val slugRuntimeJavaOpts  = EnvironmentVariables.slugRuntimeJavaOpts
+  private val environmentVariables = EnvironmentVariables.all
 
-  private lazy implicit val system: ActorSystem    = ActorSystem()
-  private lazy implicit val mat: ActorMaterializer = ActorMaterializer()
+  private implicit val system: ActorSystem    = ActorSystem()
+  private implicit val mat: ActorMaterializer = ActorMaterializer()
 
-  private lazy val progressReporter = new ProgressReporter()
-  private lazy val httpClient       = StandaloneAhcWSClient()
-  private lazy val fileDownloader   = new FileDownloader(httpClient)
-  private lazy val artifactoryConnector =
+  private val progressReporter = new ProgressReporter()
+  private val httpClient       = StandaloneAhcWSClient()
+  private val fileDownloader   = new FileDownloader(httpClient)
+  private val artifactoryConnector =
     new ArtifactoryConnector(
       httpClient,
       fileDownloader,
